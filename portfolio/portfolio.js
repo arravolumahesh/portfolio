b5b97b9c3a05ab4faab1e3aa311f7a573a455dcb
arravolumahesh <arravolumahesh@gmail.com>
2023-02-24 23:02:27 +0530

    commit

---
+++ portfolio/portfolio.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Python Developer", "Frontend Developer","Web Developer","Web Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+
+
+
